049685b892e5e06e7de5e7215c992fec5e5ae1fe
Richard Calderan <richardcck@hotmail.com>
2021-07-25 13:56:38 -0300

    docker compose

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,9 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
+            
             <p className="text-center">You have no todo items yet! Add one above!</p>
+
             )}
             {items.map(item => (
                 <ItemDisplay
